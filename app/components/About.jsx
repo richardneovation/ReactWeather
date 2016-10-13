f76cd9c6f717209f6233cc815933a7c82ecb6f3e
Richard Blais <richard.blais@neovation.local>
2016-10-13 14:43:47 -0500

    Rename about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h2>About Component</h2>
+    <div>
+      <h2>Examples</h2>
+      <p>Welcome to examples page!</p>
+    </div>
   );
 };
 

2ccb16cbd00bc25d116224a9f13e36d043003e7b
Richard Blais <richard.blais@neovation.local>
2016-10-13 15:52:12 -0500

    Adding styling

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,15 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h2>Examples</h2>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li><Link to='/?location=Winnipeg'>Winnipeg, MB</Link></li>
+        <li><Link to='/?location=Regina'>Regina, SK</Link></li>
+      </ol>
     </div>
   );
 };
